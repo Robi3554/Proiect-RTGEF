9b26a2d71561a2f7c44a3b538f040a5274794c8c
Robi3554 <rvarga253@yahoo.com>
2024-10-15 16:23:52 +0300

    Background is smooth

--- Assets/Scripts/Background/BackgroundLoop.cs
+++ Assets/Scripts/Background/BackgroundLoop.cs
@@ -7,17 +7,17 @@ public class BackgroundLoop : MonoBehaviour
 {
     private Transform cameraTransform;
 
-    private Vector3 lastCamPos;
-
     private float textureUnitSizeX;
     private float textureUnitSizeY;
 
     void Start()
     {
         cameraTransform = Camera.main.transform;
-        lastCamPos = cameraTransform.position;
+
         Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+
         Texture2D texture = sprite.texture;
+
         textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
         textureUnitSizeY = texture.height / sprite.pixelsPerUnit;
     }
@@ -26,14 +26,12 @@ public class BackgroundLoop : MonoBehaviour
     {
         if(Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
         {
-            float offsetPositionX = (cameraTransform.position.x - transform.position.x) / textureUnitSizeX;
-            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+            transform.position = new Vector3(cameraTransform.position.x , transform.position.y);
         }
 
         if (Mathf.Abs(cameraTransform.position.y - transform.position.y) >= textureUnitSizeY)
         {
-            float offsetPositionY = (cameraTransform.position.y - transform.position.y) / textureUnitSizeY;
-            transform.position = new Vector3(transform.position.x, cameraTransform.position.y + offsetPositionY);
+            transform.position = new Vector3(transform.position.x, cameraTransform.position.y);
         }
     }
 
