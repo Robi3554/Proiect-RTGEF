847cf14ada5713c42f9b1d5a03fad1dc11cdca97
Robert Varga <rvarga253@yahoo.com>
2024-11-25 17:11:34 +0200

    Added pause menu

---
+++ Assets/Scripts/Menus&Options/PauseMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject panel;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameManager.Instance.isPaused)
+            {
+                UnPause();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        panel.SetActive(true);
+        Time.timeScale = 0f;
+        GameManager.Instance.isPaused = true;
+    }
+
+    public void UnPause()
+    {
+        panel.SetActive(false);
+        Time.timeScale = 1f;
+        GameManager.Instance.isPaused = false;
+    }
+}
